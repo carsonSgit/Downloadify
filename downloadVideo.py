3e0bd0033020dd1c156c0f0a3e7e2a2fc036246a
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2023-12-13 00:08:34 -0500

    Imported Python Script

---
+++ downloadVideo.py
@@ -0,0 +1,95 @@
+#############################################################################################
+# Downloadify                                                                               #
+#                                                                                           #
+# By: Carson Spriggs-Audet                                                                  #
+#                                                                                           #
+# Date: 2021-08-10                                                                          #
+#                                                                                           #
+# Description: A very simple YouTube video downloader                                       #           
+#        inspired by Victor Kamau's tutorial (source below).                                #
+#############################################################################################
+
+"""
+Necessary pip installs:
+pip install PyTube
+pip install customtkinter
+pip install Tkinter
+"""
+
+"""
+Credits and Sources:
+
+1. Tutorial for the foundation of the application:
+   YouTube Video Downloader using Python by Victor Kamau
+   Source: https://www.section.io/engineering-education/youtube-video-downloader-using-python/
+
+2. Inspiration for enhancing the application's appearance:
+   Modern GUI using Tkinter
+   Source: https://medium.com/@fareedkhandev/modern-gui-using-tkinter-12da0b983e22
+
+   CustomTKinter Documentation
+   Source: https://customtkinter.tomschimansky.com/documentation/color
+"""
+
+
+# Imports
+import os
+import tkinter as tk
+import customtkinter
+from tkinter import Label, Entry, StringVar, Button
+from pytube import YouTube
+
+customtkinter.set_default_color_theme("green") 
+
+class Downloadify:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry('500x250')
+        self.root.resizable(True,True)
+        self.root.title('Downloadify')
+        self.link_var = StringVar()
+
+        # Header text
+        label_font = customtkinter.CTkFont(family='Inter', size=15, weight='bold')
+        label = customtkinter.CTkLabel(self.root, text="Paste your chosen URL", font=label_font)
+        label.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+
+        # URL input box
+        entry = customtkinter.CTkEntry(self.root, width=300, textvariable=self.link_var)
+        entry.place(relx=0.5, rely=0.35, anchor=tk.CENTER)
+
+        # Download button
+        button_font = customtkinter.CTkFont(family='Inter', size=16, weight='bold')
+        # On download button press, call the download function
+        button = customtkinter.CTkButton(master=root, text='Download', font=button_font, command=self.download)
+        button.place(relx=0.5, rely=0.55, anchor=tk.CENTER)
+
+    def download(self):
+        # Try catch block to catch errors
+        try:
+            # Download the video using pytube
+            url = YouTube(str(self.link_var.get()))
+            # Format as an mp4 for best quality, if mp4 isn't available it goes to pytube's default
+            video = url.streams.filter(file_extension='mp4').first()
+            # Place it in the downloads folder
+            video.download(downloads_folder)
+            
+            # Success message
+            label_font = customtkinter.CTkFont(family='Inter', size=15, weight='bold')
+            label = customtkinter.CTkLabel(self.root, text="Done!", font =label_font)
+            label.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
+
+        # Error caught (No URL inputted/Not a YouTube URL)
+        except Exception as e:
+            # Error message text display
+            label_font = customtkinter.CTkFont(family='Inter', size=15, weight='bold')
+            label = customtkinter.CTkLabel(self.root, text="Error, paste a valid YouTube URL", font =label_font)
+            label.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
+
+# Find user's download folder
+downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+# Create the main window
+root = customtkinter.CTk()
+app = Downloadify(root)
+root.mainloop()
